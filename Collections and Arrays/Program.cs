f26b4555cb4ace7bfec3a0dc315f5d64f672135d
Richard Z Troester <rzachprime@gmail.com>
2017-10-19 02:26:00 -0400

    added an int array to show how an array is similar to a collection.

--- Collections and Arrays/Program.cs
+++ Collections and Arrays/Program.cs
@@ -10,6 +10,29 @@ namespace Collections_and_Arrays
     {
         static void Main(string[] args)
         {
+            //create a collection that is a list of strings
+            var dogs = new List<string>();
+
+            dogs.Add("Bulldog");
+            dogs.Add("Collie");
+            dogs.Add("Retriever");
+
+            //foreach to move through the list
+            foreach (var dog in dogs)
+            {
+                //display each dog on a different line in a console window
+                Console.WriteLine(dog + " ");
+            }
+
+            //create an array of intergers
+
+            int[] a1 = new int[] { 1, 3, 5, 7, 9, 11 };
+
+            foreach (int i in a1)
+            {
+                Console.WriteLine(i + " ");
+            }
+
         }
     }
 }
